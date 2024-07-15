b883c4b158e33fe91b2149d74f20fdb4b6a8d7a8
RickytheGuy <rickythepsguy@gmail.com>
2024-07-15 12:14:46 -0500

    updated for danielle

--- server/server.py
+++ server/server.py
@@ -1,18 +1,34 @@
-from flask import Flask, request
+# Import flask class. Flask helps handle HTTP requests. In this case it routes the GET requests. It also, might be why you see who makes a request to the server and when since it aids in it.
+from flask import Flask, request, make_response
+# Calling the ServerData class in sever_data.py. This is the class that will be used to get the names and ids of the candidates.
 from sever_data import ServerData
 
+# Setting up flask app. Creating an instance of Flask class.
 app = Flask(__name__)
 
+# Kind confused but almost get it. Creating an instance of ServerData class by making an object of it, but only if the script is run directly. I think this is to make sure the data is loaded when the server is started. It blocks it to this local block though.
 if __name__ == '__main__':
     sd = ServerData()
 
+# @app.route binds this function to the url, so it will run if the url is searched from anywhere. It maps the url to here. If someone successfully sends a GET request to this server, ending /data, it will return an HTML message, "This is my message to Danielle!!!", and a status code of 200.
 @app.route('/data', methods=['GET'])
 def send_data():
     return "This is my message to Danielle!!!", 200
 
+
+# Similar process, but more specific. If someone sends a GET request to the server, ending in /name_id, it will call an instance for a function that will return the names and ids of the candidates from the get_names function in the sd class. 
 @app.route('/name_id', methods=['GET'])
 def send_name_id():
-    return sd.get_names(), 200
+    return sd.get_name_id(), 200
 
+@app.route('/get_candidate', methods=['GET'])
+def get_candidate():
+    candidate_id = request.args.get('candidate_id', type=int)
+    if candidate_id:
+        data, code = sd.get_candidate(candidate_id)
+        return make_response(data, code)
+    return f"Bad request. Please provide a candidate_id. '{candidate_id}' not understood", 400
+
+# If the script is run directly, the server will run on the host on port 5000. THis is what makes it accessable from other devices on the same network.
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- server/sever_data.py
+++ server/sever_data.py
@@ -1,11 +1,48 @@
+# os module allows us to interact with the file paths on the computer.
 import os
-
+# I think this will help manipulate the cvs file to make it more readable.
 import pandas as pd
 
+# The CVS file has the list of all the cadidates and their IDs. Built a class to get the names and IDs of the candidates. 
 class ServerData():
+    # This line initializes objects of the class when they're made. Makes the object but doesn't return anything.
     def __init__(self) -> None:
+        # This line finds and reads the CVS file and stores it in a Pnadas dataframe assigned as the basic_data attribute in ServerData class.
         self.basic_data = pd.read_csv(os.path.join('data', 'candidate_basic.csv'))
     
-    def get_names(self) -> str:
+    # This function takes in self and returns a string of the names and IDs of the candidates in the basic_data attribute in JSON records format.
+    def get_name_id(self) -> str:
         return self.basic_data[['name', 'candidate_id']].to_json(orient='records')
     
+    def get_candidate(self, candidate_id: int) -> tuple:
+        """
+        Return a dictionary of the candidate's data and a status code.
+        For now we us
+        """
+        try:
+            name = self.basic_data.loc[self.basic_data['candidate_id'] == candidate_id, 'name'].values[0]
+        except:
+            name = 'TEST NAME'
+        return {
+            'candidate_id': candidate_id,
+            'name': name,
+            'party': 'Republican',
+            'state': 'Texas',
+            'district': 1,
+            'age': 45,
+            'sex': 'M',  
+            'incumbent': False,
+            'abortion': 'Pro-life', # Is it a string?
+            'gun_control': 2, # Scale of 1-5?
+            'foregin_policy': '',
+            'rich_pay_students' : [''], # List of strings?
+            'rich_pay_students' : [''],
+            'rich_pay_students' : [''],
+            'rich_pay_students' : [''],
+            'rich_pay_students' : [''],
+            'rich_pay_students' : [''],
+            'rich_pay_students' : [''],
+            'rich_pay_students' : [''],
+            'rich_pay_students' : [''],
+            'rich_pay_students' : [''],
+        }, 200
